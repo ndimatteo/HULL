5426bfc7540daa032f2066feff75f5da4d5cb44b
Nick DiMatteo <hello@nickdimatteo.com>
2020-11-09 15:56:43 -0700

    add focus lock

--- components/drawer.js
+++ components/drawer.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import { motion, AnimatePresence } from 'framer-motion'
+import FocusTrap from 'focus-trap-react'
 
 const Drawer = ({ title, open, toggle, children }) => {
   if (typeof document === `undefined`) {
@@ -20,46 +21,48 @@ const Drawer = ({ title, open, toggle, children }) => {
   return ReactDOM.createPortal(
     <AnimatePresence initial={false}>
       {open && (
-        <>
-          <motion.div
-            key="drawerBG"
-            initial="hidden"
-            animate="visible"
-            exit="hidden"
-            variants={{
-              visible: { opacity: 1 },
-              hidden: { opacity: 0 },
-            }}
-            transition={{ duration: 0.4, ease: [0.19, 1.0, 0.22, 1.0] }}
-            className="drawer--backdrop"
-            onClick={() => toggle(false)}
-          />
-          <motion.nav
-            key="drawer"
-            initial="closed"
-            animate="open"
-            exit="closed"
-            variants={{
-              open: { x: '0%' },
-              closed: { x: '100%' },
-            }}
-            transition={{ duration: 0.5, ease: [0.19, 1.0, 0.22, 1.0] }}
-            className="drawer"
-          >
-            <div className="drawer--inner">
-              <div className="drawer--header">
-                {title && <div className="drawer--title">{title}</div>}
-                <button
-                  className="btn drawer--close"
-                  onClick={() => toggle(false)}
-                >
-                  Done
-                </button>
+        <FocusTrap>
+          <div className="drawer--wrapper">
+            <motion.div
+              key="drawerBG"
+              initial="hidden"
+              animate="visible"
+              exit="hidden"
+              variants={{
+                visible: { opacity: 1 },
+                hidden: { opacity: 0 },
+              }}
+              transition={{ duration: 0.4, ease: [0.19, 1.0, 0.22, 1.0] }}
+              className="drawer--backdrop"
+              onClick={() => toggle(false)}
+            />
+            <motion.nav
+              key="drawer"
+              initial="closed"
+              animate="open"
+              exit="closed"
+              variants={{
+                open: { x: '0%' },
+                closed: { x: '100%' },
+              }}
+              transition={{ duration: 0.5, ease: [0.19, 1.0, 0.22, 1.0] }}
+              className="drawer"
+            >
+              <div className="drawer--inner">
+                <div className="drawer--header">
+                  {title && <div className="drawer--title">{title}</div>}
+                  <button
+                    className="btn drawer--close"
+                    onClick={() => toggle(false)}
+                  >
+                    Done
+                  </button>
+                </div>
+                <div className="drawer--content">{children}</div>
               </div>
-              <div className="drawer--content">{children}</div>
-            </div>
-          </motion.nav>
-        </>
+            </motion.nav>
+          </div>
+        </FocusTrap>
       )}
     </AnimatePresence>,
     document.querySelector('#drawer')
